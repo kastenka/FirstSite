81a4597400f4ff5c9aa43d6f7f0b3f6644b95d98
Kastenka <kastenka.whatsup@gmail.com>
2022-03-26 18:32:12 +0300

    Add first Django project

--- testsite/testsite/news/views.py
+++ testsite/testsite/news/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    print(request.META['USERNAME'])
-    return HttpResponse("Hello World")
-
-
-def test(request):
-    return HttpResponse("<h1>Test Page</h1>")
+    news = News.objects.all().order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'List of news'
+    }
+    return render(request, 'news/index.html', context)
