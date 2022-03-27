e233d751593e86a17a4aa7bd1dbb8242f85a0612
Kastenka <kastenka.whatsup@gmail.com>
2022-03-27 11:02:13 +0300

    Edit admin part

--- testsite/testsite/news/models.py
+++ testsite/testsite/news/models.py
@@ -4,10 +4,18 @@ from django.db import models
 class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Create')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Update')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='Published')
+    category_id = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'My news'
+        verbose_name_plural = 'News'
+        ordering = ['created_at', 'title']
+
+
