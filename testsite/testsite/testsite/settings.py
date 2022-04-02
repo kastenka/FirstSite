4086ab549936ea1175a4d3a4508cc0385d634de9
Kastenka <kastenka.whatsup@gmail.com>
2022-04-02 19:33:50 +0300

    Add static files

--- testsite/testsite/testsite/settings.py
+++ testsite/testsite/testsite/settings.py
@@ -116,6 +116,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'testsite/static')
+]
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
